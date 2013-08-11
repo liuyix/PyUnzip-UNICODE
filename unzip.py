a9b536052b839d4f86ec52c0e93503ac5c157e50
liuyix <hi@liuyix.org>
2013-08-11 10:44:06 +0800

    refactor the code and make `--list` option

--- unzip.py
+++ unzip.py
@@ -1,34 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Last modified: 2011 Oct 21 10:49:20 AM CST
-#
-# LICENSE:
-#   Copyright (c) 2010 Tzeng, Yi-Feng
-#
-#   Permission is hereby granted, free of charge, to any person obtaining
-#   a copy of this software and associated documentation files (the
-#   "Software"), to deal in the Software without restriction, including
-#   without limitation the rights to use, copy, modify, merge, publish,
-#   distribute, sublicense, and/or sell copies of the Software, and to
-#   permit persons to whom the Software is furnished to do so, subject to
-#   the following conditions:
-#
-#   The above copyright notice and this permission notice shall be
-#   included in all copies or substantial portions of the Software.
-#
-#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-#   LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-#   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-#   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-"""
-   Extract zip tool.
-
-   Due to default unzip in linux platfrom can not extract cp950, cp936 encoding etc.
-"""
 
 __version__ = "1.0"
 __revision__ = '1.2.1'
@@ -45,27 +16,73 @@ import shutil
 DEFAULT_ENCODING = 'cp936'
 
 def usage():
-    print """usage: unzip.py <zipfile> [[-e <encoding>] -p <password>]
+    print """usage: unzip.py [-l] <zipfile> [[-e <encoding>] -p <password>]
     <zipfile> is the source zipfile to extract
     <encoding> is the encoding of zipfile
     <password> is the password of zipfile
 
     -h: help
-
+    -l: list files only
     long options also work:
     --verbose
     --encoding
     --password
-
+    --list 
     """
 
+def get_filename(fileinfo, encoding):
+    """
+    Return Values:
+    	return filename on success or None otherwise
+    """
+    if encoding == None:
+        return fileinfo.filename
+    try:
+        filename = unicode(fileinfo.filename, encoding)
+    except TypeError:
+        filename = fileinfo.filename
+    except:
+        #print "ERROR: unknown encoding (" + encoding + ")"
+        filename = None
+    return filename
+    
+def get_zipinfo_unicode(fileinfo, encoding=None):
+    assert fileinfo != None
+    filename = get_filename(fileinfo, encoding)
+    file_size = fileinfo.file_size
+    compress_size = fileinfo.compress_size
+    crc_value = fileinfo.CRC
+    date_time = fileinfo.date_time
+    fileinfo_dict = {"filename": filename, "file_size": file_size, "compress_size": compress_size, "datetime": date_time}
+    return fileinfo_dict
+    pass
+
+def pretty_datetime(datetime):
+    return str(datetime[0]).zfill(2) + "-" + str(datetime[1]).zfill(2) + "-" + str(datetime[2]).zfill(2) + " " + str(datetime[3]).zfill(2) + ":" + str(datetime[4]).zfill(2) + ":" + str(datetime[5]).zfill(2)
+    
+ZIPINFO_DESC = "{:>10}  {:>10}   {:>10}  FILE_NAME".format("压缩后", "解压后", "时间")
+    
+def zipinfo_prettify(fileinfo, encoding=None):
+    infos = get_zipinfo_unicode(fileinfo, encoding)
+    keys = ['filename', 'compress_size', 'file_size', 'datetime']
+    info_str = ''
+    for key in keys[1:]:
+        if key == 'datetime':
+            s = pretty_datetime(infos[key])
+        else:
+            s = str(infos[key])
+        info_str += "{:>10}  ".format(s)
+    info_unicode = info_str
+    info_unicode = unicode(info_str, "ascii") + unicode("\t", "ascii") + infos['filename']
+    return info_unicode
+
 def main():
-    shortargs = 'he:p:'
-    longargs = ['help', 'encoding=', 'password=']
+    shortargs = 'hel:p:'
+    longargs = ['help', 'encoding=', 'password=', 'list']
 
     if len(sys.argv) < 2:
         usage()
-        sys.exit()
+        sys.exit(0)
 
     try:
         if sys.argv[1].startswith('-'):
@@ -80,6 +97,7 @@ def main():
 
     encoding = DEFAULT_ENCODING
     password = None
+    list_file = False
 
     for o, a in opts:
         if o in ("-h", "--help"):
@@ -89,7 +107,8 @@ def main():
             encoding = a
         if o in ("-p", "--password"):
             password = a
-
+        if o in ("-l", "--list"):
+            list_file = True
     try:
         f = zipfile.ZipFile(zipsource, 'r')
     except zipfile.BadZipfile:
@@ -99,28 +118,29 @@ def main():
     if password != None:
         f.setpassword(password)
 
+    if list_file:
+        print ZIPINFO_DESC
     for fileinfo in f.infolist():
-        try:
-            filename = unicode(fileinfo.filename, encoding)
-        except TypeError:
-            filename = fileinfo.filename
-        except:
+        filename = get_filename(fileinfo, encoding)
+        if filename == None:
             print "ERROR: unknown encoding (" + encoding + ")"
             sys.exit(2)
-
-        if filename.endswith('/'):
-            if not os.path.isdir(filename):
-                os.mkdir(filename)
-                print "Create : " + filename
+        if list_file:
+            #print filename
+            print zipinfo_prettify(fileinfo, encoding)
         else:
-            outputfile = open(filename, "wb")
-            try:
-                shutil.copyfileobj(f.open(fileinfo.filename), outputfile)
-            except:
-                print "ERROR: File is encrypted, password required for extraction (-p, --password)"
-                sys.exit(2)
-
-            print "Extract: " + filename
+            if filename.endswith('/'):
+                if not os.path.isdir(filename):
+                    os.mkdir(filename)
+                    print "Create : " + filename
+                else:
+                    outputfile = open(filename, "wb")
+                    try:
+                        shutil.copyfileobj(f.open(fileinfo.filename), outputfile)
+                    except:
+                        print "ERROR: File is encrypted, password required for extraction (-p, --password)"
+                        sys.exit(2)
+                print "Extract: " + filename
 
 if __name__ == '__main__': main()
 
